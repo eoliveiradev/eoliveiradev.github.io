6ef1251ddd66c7c58503208c0dc718d9360aacda
Emanuel-Oliveira <emanuel.oliveira.contato@gmail.com>
2022-12-03 22:10:58 -0300

    :sparkles: Default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,43 @@
+export const DefaultTheme = {
+  COLORS: {
+    BACKGROUND: "#121214",
+    NEUTRAL: "#E1E1E6",
+    TEXT: '#F2F2FA',
+    PRIMARY: '#4EA8DE',
+    
+    BUTTON: {
+      PRIMARY: '#4EA8DE',
+      HOVER: '#68b4e3',
+    }
+  },
+
+  SIZES: {
+    CONTENT_WIDTH: "1440px",
+    HEADER_HEIGHT: "80px",
+
+    FONT: {
+      LG: '2.5rem',
+      MD: '2rem',
+      SM: '1.5rem',
+
+      MOBILE: {
+        LG: '1.5rem',
+        MD: '1.25rem',
+        SM: '1rem',
+      }
+    }
+  },
+
+  FONTS: {
+    PRIMARY: "'Roboto', sans-serif",
+
+    WEIGTHS: {
+      NORMAL: 500,
+      BOLD: 700
+    }
+  },
+
+  BORDERRADIUS: {
+    DEFAULT: '8px'
+  }
+}
